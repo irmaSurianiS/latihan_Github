6a6f5382f83dec108bd831fad8e1c89880d66d0f
irmaSurianiS <97292016+irmaSurianiS@users.noreply.github.com>
2022-10-26 21:39:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Belajar Github</title>
+</head>
+<body>
+    <?php 
+    echo "Belajar Github!";
+    echo "Irma Suriani S (60900120043)";
+     ?>
+</body>
+</html>
